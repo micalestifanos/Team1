a3ea46463fa994dd9962090a94cad9b4f154c2bd
Luccas <luccaspg@gmail.com>
2019-03-12 15:00:10 -0400

    fixes

--- client/js/factories/chartInit.js
+++ client/js/factories/chartInit.js
@@ -1,6 +1,8 @@
 google.charts.load('current', {'packages':['corechart']});
+google.charts.load('current', {'packages':['table']});
 google.charts.setOnLoadCallback(drawFirstChart);
 google.charts.setOnLoadCallback(drawSecondChart);
+google.charts.setOnLoadCallback(drawTable);
 
 
 function drawFirstChart(jsonData) {
@@ -46,4 +48,24 @@ function drawSecondChart() {
 
     var chart = new google.visualization.PieChart(document.getElementById('second-chart'));
     chart.draw(data, options);
+  }
+
+
+ 
+
+  function drawTable() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Name');
+    data.addColumn('number', 'Salary');
+    data.addColumn('boolean', 'Full Time Employee');
+    data.addRows([
+      ['Mike',  {v: 10000, f: '$10,000'}, true],
+      ['Jim',   {v:8000,   f: '$8,000'},  false],
+      ['Alice', {v: 12500, f: '$12,500'}, true],
+      ['Bob',   {v: 7000,  f: '$7,000'},  true]
+    ]);
+
+    var table = new google.visualization.Table(document.getElementById('table_div'));
+
+    table.draw(data, {showRowNumber: true, width: '100%', height: '100%'});
   }
