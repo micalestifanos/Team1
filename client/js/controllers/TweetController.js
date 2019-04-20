aec0356bb2ebeef27e41e0d7aff0b3ae26f76327
Luccas <luccaspg@gmail.com>
2019-04-20 17:06:47 -0400

    bug on type fixed

--- client/js/controllers/TweetController.js
+++ client/js/controllers/TweetController.js
@@ -1,5 +1,10 @@
 angular.module('twitter').controller('TwitterController', ['$scope', 'Twitter',
   function($scope, Twitter) {
+    $scope.types = [
+      {type : "Popular", search : "popular"},
+      {type : "Recent", search : "recent"},
+      {type : "Mixed", search : "mixed"}
+  ];
     /* Initialize showing the Global Trend */
     var initCharts = Twitter.getGlobalTrends().then(function(response) {
       console.log(response.data);
@@ -18,10 +23,10 @@ angular.module('twitter').controller('TwitterController', ['$scope', 'Twitter',
     });
   // DOES NOT WORK WITH "#"
   $scope.searchTweet = function(){
-    if($scope.searchWord.location != undefined){
-      Twitter.search($scope.searchWord.text, $scope.searchWord.type).then(function(response){
-        $scope.search = response.data;
-        console.log("Search for:" + $scope.searchWord.text);
+    if($scope.searchWord.location == undefined){
+      Twitter.search($scope.searchWord.text, $scope.selectedType).then(function(response){
+        $scope.search = response.data; 
+        console.log("Search for:" + $scope.searchWord.text + "Search Type: " +  $scope.selectedType);
         console.log($scope.searchWord.location);
         console.log(response.data);
       }, function(error) {
@@ -30,7 +35,7 @@ angular.module('twitter').controller('TwitterController', ['$scope', 'Twitter',
 
     }
     else{
-      Twitter.searchByLocation($scope.searchWord.text, $scope.searchWord.location).then(function(response){
+      Twitter.searchByLocation($scope.searchWord.text, $scope.selectedType, $scope.searchWord.location).then(function(response){
         $scope.search = response.data;
         console.log("Search for:" + $scope.searchWord.text);
         console.log($scope.searchWord.location);
