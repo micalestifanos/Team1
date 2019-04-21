5919b9a34f04e03ee1ebce9db55c8b63d7aa2ac9
Geraldina <zhanggeraldina@gmail.com>
2019-04-21 17:51:53 -0400

    content except title resizes & did graphs

--- client/js/factories/chartInit.js
+++ client/js/factories/chartInit.js
@@ -1,14 +1,18 @@
 google.charts.load('current', {'packages':['corechart', 'table']});
 // google.charts.load('current', {'packages':['table']});
-// google.charts.setOnLoadCallback(updateCharts);
+ //google.charts.setOnLoadCallback(updateCharts);
 // google.charts.setOnLoadCallback();
-// google.charts.setOnLoadCallback(drawSecondChart);
-// google.charts.setOnLoadCallback(drawTable);
+ google.charts.setOnLoadCallback(drawTable);
+ google.charts.setOnLoadCallback(drawSecondChart);
+ google.charts.setOnLoadCallback(drawBarChart);
 
 function updateCharts(jsonData){
     // drawFirstChart(jsonData);
     // drawSecondChart(jsonData);
     drawTable(jsonData);
+    //drawFirstChart(jsonData);
+    drawBarChart(jsonData);
+    drawSecondChart(jsonData);
 }
 
 function drawFirstChart(jsonData) {
@@ -17,7 +21,7 @@ function drawFirstChart(jsonData) {
     data.addColumn('number', 'Volume');
     if(jsonData != undefined){
         for(i = 1; i < jsonData.length; i++){
-            
+
             data.addRows([
             [jsonData[i].topic, jsonData[i].volume]
             ]);
@@ -33,6 +37,7 @@ function drawFirstChart(jsonData) {
     chart = new google.visualization.Histogram(document.getElementById("time-chart"));
     chart.draw(data, options);
 }
+
 function drawSecondChart(jsonData) {
 
     var data = new google.visualization.DataTable();
@@ -40,7 +45,7 @@ function drawSecondChart(jsonData) {
     data.addColumn('number', 'Volume');
     if(jsonData != undefined){
         for(i = 1; i < jsonData.length; i++){
-            
+
             data.addRows([
             [jsonData[i].topic, jsonData[i].volume]
             ]);
@@ -50,15 +55,47 @@ function drawSecondChart(jsonData) {
 
 
     var options = {title:'Topics',
-                   width:400,
-                   height:300};
+                   width:600,
+                   height:500,
+                   pieSliceText: "none" //removes percentages because don't appear well on front end
+                 };
 
 
-    var chart = new google.visualization.PieChart(document.getElementById('second-chart'));
+    var chart = new google.visualization.PieChart(document.getElementById('pie-chart'));
     chart.draw(data, options);
   }
 
+function drawBarChart(jsonData) {
 
+  var data = google.visualization.arrayToDataTable([
+        [{label: 'Topic', type: 'string'},
+         {label: 'Volume', type: 'number'}
+      ]
+      ]);
+      if (jsonData != undefined) {
+        for (i = 1; i < jsonData.length; i++) {
+          data.addRows([
+            [jsonData[i].topic, jsonData[i].volume]
+          ]);
+        }
+      }
+
+
+      var view = new google.visualization.DataView(data);
+
+
+      var options = {
+        title: "Volume of Trending Twitter Topics",
+        width: 800,
+        height: 600,
+        bar: {groupWidth: "95%"},
+        legend: { position: "none" },
+        hAxis: { title: 'Topic', textPosition: 'none' },
+        vAxis: { title: 'Volume'}
+      };
+      var chart = new google.visualization.ColumnChart(document.getElementById("bar-chart"));
+      chart.draw(view, options);
+}
 
 
   function drawTable(jsonData) {
@@ -67,7 +104,7 @@ function drawSecondChart(jsonData) {
     data.addColumn('number', 'Volume');
     if(jsonData != undefined){
         for(i = 1; i < jsonData.length; i++){
-            
+
             data.addRows([
             [jsonData[i].topic, jsonData[i].volume]
             ]);
