a2cb480fae29a7aece1f218efb96deb4158f40e1
geraldinaz <32301747+geraldinaz@users.noreply.github.com>
2019-04-23 16:36:08 -0400

    hard code charts

--- client/js/factories/chartInit.js
+++ client/js/factories/chartInit.js
@@ -3,9 +3,8 @@ google.charts.load('current', {'packages':['corechart', 'table']});
  //google.charts.setOnLoadCallback(updateCharts);
 // google.charts.setOnLoadCallback();
 //  google.charts.setOnLoadCallback(drawTable);
-//  google.charts.setOnLoadCallback(drawSecondChart);
-//  google.charts.setOnLoadCallback(drawBarChart);
-
+    google.charts.setOnLoadCallback(drawBarChart);
+    google.charts.setOnLoadCallback(drawSecondChart);
 // function updateCharts(jsonData){
 //     // drawFirstChart(jsonData);
 //     // drawSecondChart(jsonData);
@@ -40,9 +39,24 @@ function drawFirstChart(jsonData) {
 
 function drawSecondChart(jsonData) {
 
-    var data = new google.visualization.DataTable();
-    data.addColumn('string', 'Trend');
-    data.addColumn('number', 'Volume');
+    var data = google.visualization.arrayToDataTable([
+      ["Topic", "Volume"],
+      ["#ElDebateDecisivo", 66832],
+      ["#DebateAtresmedia", 47689],
+      ["#TVOI", 10118],
+      ["Shane Long", 18546],
+      ["Chiefs", 40980],
+      ["#WorldBookDay", 122732],
+      ["#LulaLivreJá", 18977],
+      ["#CSKvSRH", 51181],
+      ["#Sinceramente", 20930],
+      ["#RepublicansForImpeachment", 109700],
+      ["#AlavésBarça", 12985],
+      ["#DíaDelIdioma", 10057],
+      ["#DiaInternacionalDelLibro", 23590],
+      ["#23Abr", 88149]
+    ]);
+/*
     if(jsonData != undefined){
         for(i = 1; i < jsonData.length; i++){
 
@@ -52,7 +66,7 @@ function drawSecondChart(jsonData) {
             }
 
     }
-
+*/
 
     var options = {title:'Topics',
                    width:600,
@@ -65,21 +79,40 @@ function drawSecondChart(jsonData) {
     chart.draw(data, options);
   }
 
-function drawBarChart(jsonData) {
+function drawBarChart() {
 
   var data = google.visualization.arrayToDataTable([
-        [{label: 'Topic', type: 'string'},
-         {label: 'Volume', type: 'number'}
-      ]
-      ]);
-      if (jsonData != undefined) {
-        for (i = 1; i < jsonData.length; i++) {
-          data.addRows([
-            [jsonData[i].topic, jsonData[i].volume]
-          ]);
-        }
-      }
+    ["Topic", "Volume"],
+    ["#ElDebateDecisivo", 66832],
+    ["#DebateAtresmedia", 47689],
+    ["#TVOI", 10118],
+    ["Shane Long", 18546],
+    ["Chiefs", 40980],
+    ["#WorldBookDay", 122732],
+    ["#LulaLivreJá", 18977],
+    ["#CSKvSRH", 51181],
+    ["#Sinceramente", 20930],
+    ["#RepublicansForImpeachment", 109700],
+    ["#AlavésBarça", 12985],
+    ["#DíaDelIdioma", 10057],
+    ["#DiaInternacionalDelLibro", 23590],
+    ["#23Abr", 88149]
+  ]);
 
+  /*
+  var string = JSON.stringify(jsonData);
+  console.log(jsonData);
+
+  if(jsonData != undefined) {
+      for(i = 1; i < jsonData.length; i++){
+
+          data.addRows([
+          [string[i].topic, string[i].volume]
+          ]);
+          }
+
+  }
+*/
 
       var view = new google.visualization.DataView(data);
 
