eda78801edce516e207a9cfbeb81090742568f5a
Steven Medina <stevenmedina@ufl.edu>
2019-04-22 20:53:19 -0400

    Some frontend bug fixes

--- client/js/factories/TweetFactory.js
+++ client/js/factories/TweetFactory.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 angular.module('twitter', []).factory('Twitter', function ($http) {
   var methods = {
     getGlobalTrends: function (token) {
@@ -6,24 +5,6 @@ angular.module('twitter', []).factory('Twitter', function ($http) {
         $http.defaults.headers.common.Authorization = token;
       } else {
         delete $http.defaults.headers.common.Authorization;
-=======
-angular.module('twitter', []).factory('Twitter', function($http) {
-    var methods = {
-      getGlobalTrends: function() {
-        return $http.get('/api/twitter');
-        // $http.get('/apit/twitter')
-        // .then(function(res){
-        //   return res;
-        // }, function(err){
-        //   console.log(err);
-        // });
-        // .success(function(data){
-        //   return data;
-        // })
-        // .error(function(res){
-        //   console.log("Error Requesting Data" + res);
-        // });
->>>>>>> luccas_sprint2
       }
       return $http.get('/api/twitter');
     },
--- client/js/factories/chartInit.js
+++ client/js/factories/chartInit.js
@@ -1,24 +1,13 @@
-<<<<<<< HEAD
 google.charts.load('current', { 'packages': ['corechart', 'table'] });
-=======
-
-google.charts.load('current', {'packages':['corechart', 'table']});
->>>>>>> luccas_sprint2
 // google.charts.load('current', {'packages':['table']});
 // google.charts.setOnLoadCallback(updateCharts);
 // google.charts.setOnLoadCallback();
 // google.charts.setOnLoadCallback(drawSecondChart);
 // google.charts.setOnLoadCallback(drawTable);
 
-<<<<<<< HEAD
 function updateCharts(jsonData) {
     // drawFirstChart(jsonData);
     // drawSecondChart(jsonData);
-=======
-function updateCharts(jsonData){
-    drawFirstChart(jsonData);
-    drawSecondChart(jsonData);
->>>>>>> luccas_sprint2
     drawTable(jsonData);
 }
 
@@ -26,15 +15,9 @@ function drawFirstChart(jsonData) {
     var data = new google.visualization.DataTable();
     data.addColumn('string', 'Trend');
     data.addColumn('number', 'Volume');
-<<<<<<< HEAD
     if (jsonData != undefined) {
         for (i = 1; i < jsonData.length; i++) {
 
-=======
-    if(jsonData != undefined){
-        for(i = 1; i < jsonData.length - 1; i++){
-            
->>>>>>> luccas_sprint2
             data.addRows([
                 [jsonData[i].topic, jsonData[i].volume],
             ]);
@@ -54,15 +37,9 @@ function drawSecondChart(jsonData) {
     var data = new google.visualization.DataTable();
     data.addColumn('string', 'Trend');
     data.addColumn('number', 'Volume');
-<<<<<<< HEAD
     if (jsonData != undefined) {
         for (i = 1; i < jsonData.length; i++) {
 
-=======
-    if(jsonData != undefined){
-        for(i = 1; i < jsonData.length; i++){
-            
->>>>>>> luccas_sprint2
             data.addRows([
                 [jsonData[i].topic, jsonData[i].volume]
             ]);
@@ -83,15 +60,9 @@ function drawTable(jsonData) {
     var data = new google.visualization.DataTable();
     data.addColumn('string', 'Trend');
     data.addColumn('number', 'Volume');
-<<<<<<< HEAD
     if (jsonData != undefined) {
         for (i = 1; i < jsonData.length; i++) {
 
-=======
-    if(jsonData != undefined){
-        for(i = 1; i < jsonData.length; i++){
-            
->>>>>>> luccas_sprint2
             data.addRows([
                 [jsonData[i].topic, jsonData[i].volume]
             ]);
